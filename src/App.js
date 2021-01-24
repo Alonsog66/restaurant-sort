8d70903174aeed17ccfd7836841be8d07cc6babe
Alonso Garza <alonsogarza@Alonsos-MacBook-Pro.local>
2021-01-23 23:01:10 -0600

    Added state filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useState, useEffect, useRef } from 'react';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from './Components/Grid'
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 import './App.css';
 
@@ -16,12 +19,20 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(1),
     width: '25ch',
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 function App() {
   const name = useRef('');
   const city = useRef('');
   const genre = useRef('');
+  const state = useRef('All');
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const classes = useStyles();
@@ -42,16 +53,19 @@ function App() {
     if (filter === 'name') name.current = e.target.value;
     else if (filter === 'city') city.current = e.target.value;
     else if (filter === 'genre') genre.current = e.target.value;
+    else if (filter === 'state') state.current = e.target.value;
   }
   function filterRestaurants() {
     const filteredRestaurants = allRestaurants
-      .filter((restaurant) => restaurant.name.toLowerCase().includes(name.current.toLowerCase()))
-      .filter((restaurant) => restaurant.city.toLowerCase().includes(city.current.toLowerCase()))
-      .filter((restaurant) => restaurant.genre.toLowerCase().includes(genre.current.toLowerCase()));
+      .filter(restaurant => restaurant.name.toLowerCase().includes(name.current.toLowerCase()))
+      .filter(restaurant => restaurant.city.toLowerCase().includes(city.current.toLowerCase()))
+      .filter(restaurant => restaurant.genre.toLowerCase().includes(genre.current.toLowerCase()))
+      .filter(restaurant => (state.current === "All" || restaurant.state === state.current))
     setFilteredRestaurants(filteredRestaurants);
   }
 
-  const labels = ['name', 'city', 'genre'];
+
+  const labels = ['name', 'genre', 'city'];
   const labelComponents = labels.map((el, i) => (
     <TextField
       key={`TextField-${i}`}
@@ -61,12 +75,24 @@ function App() {
       onChange={(e) => { updateFilter(e, el); filterRestaurants(); }}
     />))
 
+  // Creating state options
+  const states = [...(new Set(allRestaurants.map(el => el.state)))];
   return (
     <div id="App">
       <header id="header">
         <h1>Restaurant Search</h1>
         <div>
           {labelComponents}
+          <FormControl className={classes.formControl}>
+            <InputLabel >State</InputLabel>
+            <Select
+              native
+              onChange={(e) => { updateFilter(e, 'state'); filterRestaurants(); }}
+            >
+              <option aria-label="None" value="All">All</option>
+              {states.map((state, i) => (<option value={state} key={`stateChoice-${i}`}>{state}</option>))}
+            </Select>
+          </FormControl>
         </div>
       </header>
       {filteredRestaurants.length > 0 ? <Grid restaurants={filteredRestaurants} /> : "No restaurants found"}
